5947c2ee3417867b8387ad4aaf812d1889088938
shatian114 <1320024819@qq.com>
2019-02-02 19:52:24 +0800

    修改了查询接口返回的数据格式，src/models/list.js,第41行

--- src/models/list.js
+++ src/models/list.js
@@ -37,6 +37,12 @@ export default {
       };
       const response = yield call(queryList, temp);
       if (response) {
+
+				//之前的api没有data元素，现在的data元素里面都是在上一级，现在提到上一级
+				for(let k in response.data){
+					response[k] = response.data[k];
+				}
+
         yield put({
           type: 'save',
           payload: {
@@ -60,6 +66,7 @@ export default {
 
   reducers: {
     save(state, { payload }) {
+			console.log('payload', payload);
       return {
         ...state,
         ...payload,
