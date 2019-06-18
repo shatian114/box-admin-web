286e30f4272735cc57c3c0646ea30c349a16d728
shatian114 <1320024819@qq.com>
2019-06-18 16:39:37 +0800

    修改了获取菜单的方式

--- src/services/api.js
+++ src/services/api.js
@@ -42,7 +42,11 @@ export async function logout() {
 // 菜单与操作权限
 
 export async function getMenuData() {
-  return request(`${SERVER_ADDR}/api/authority/getMenus`);
+  // return request(`${SERVER_ADDR}/api/authority/getMenus`);
+  return requestModels(`${SERVER_ADDR}/api/authority/getnewMenus`, {
+    method: 'POST',
+    body: {'sysid': 'Z2'},
+  });
 }
 
 export async function getOprs(params) {
